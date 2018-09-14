8cbed794dad77382c96a0b6081187d817e724ba0
natalia sokolowska <sokolowska16@gmail.com>
2018-09-14 23:13:03 +0200

    added activity 4, UI clean up, code clean up

--- app/src/main/java/com/example/ble/ble/Activity2.java
+++ app/src/main/java/com/example/ble/ble/Activity2.java
@@ -2,11 +2,8 @@ package com.example.ble.ble;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +17,8 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //button = findViewById(R.id.button);
         /*button.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +30,7 @@ public class Activity2 extends AppCompatActivity {
         });*/
 
 
-        String[] elementy = {"Beacon 1", "Beacon 2", "Beacon 1", "Beacon 2"};
+        String[] elementy = {"Beacon 1", "Beacon 2", "Beacon 3", "Beacon 4"};
 
         ArrayAdapter adapter = new ArrayAdapter(this, R.layout.elementy_listy, R.id.textView, elementy);
 
