938ffacc8b73274a4d3460ac122e9edf17130991
rafal.szymala <rafalaszymala@gmail.com>
2019-02-12 21:32:58 +0100

    added scanner binding draft and buttons to import

--- app/src/main/java/com/example/ble/ble/CustomAdapter.java
+++ app/src/main/java/com/example/ble/ble/CustomAdapter.java
@@ -1,10 +1,15 @@
 package com.example.ble.ble;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -13,12 +18,31 @@ import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter<Beacon> {
 
+    private ITagService service;
+
     public CustomAdapter(ArrayList<Beacon> data, Context context) {
         super(context, R.layout.customlayout, data);
     }
 
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder)
+        {
+            if (iBinder instanceof ITagService.BackgroundBluetoothLEBinder) {
+                service = ((ITagService.BackgroundBluetoothLEBinder) iBinder).service();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName)
+        {
+            Log.d(ITagService.TAG, "onServiceDisconnected()");
+        }
+    };
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         // Get the data item for this position
         Beacon beacon = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
@@ -33,6 +57,13 @@ public class CustomAdapter extends ArrayAdapter<Beacon> {
         textViewName.setText(beacon.getName());
         //imageView2.setBackgroundResource(beacon.getImage());
         setProgress(beacon.getBeaconRange(), progressBar);
+
+
+        Button connectButton = convertView.findViewById(R.id.button_connect);
+
+
+        Button alertButton = convertView.findViewById(R.id.button_alert);
+
 
         return convertView;
     }
